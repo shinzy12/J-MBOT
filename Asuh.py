2b11463c9c4d2e87054583b211772728d9b361a0
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-02-19 11:50:19 +0700

    Update Asuh.py

--- Asuh.py
+++ Asuh.py
@@ -519,10 +519,7 @@ def passwrd():
 					pwv.append(nmf)
 					pwv.append(frs+'123')
 					pwv.append(frs+'12345')
-			pwv.append('sayang')
-                        pwv.append('bismillah')
-                        pwv.append('anjing')
-                        pwv.append('kimcil')
+			pwv.append('sayang,bismillah,kimcil')
 			if 'mobile' in method:
 				pool.submit(crack,idf,pwv)
 			elif 'api' in method:
