aeb371d7a14a59a16515a56ec0a861f5b23ce633
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-03-09 04:02:49 +0700

    Update Asuh.py

--- Asuh.py
+++ Asuh.py
@@ -364,7 +364,7 @@ def dump_publik():
 	print(x+'['+h+'•'+x+'] Ketik "me" Jika Ingin Dump ID Dari Teman')
 	pil = input(x+'['+p+'f'+x+'] Masukkan ID Target : ')
 	try:
-		koh = requests.get('https://graph.facebook.com/'+pil+'?access_token='+token)
+		koh = requests.get('https://graph.facebook.com/%s?access_token=%s')
 		grex = json.loads(koh.text)['name']
 		kras = '# INFO TARGET'
 		kras2 = mark(kras, style='green')
@@ -382,7 +382,7 @@ def dump_publik():
 		sol().print(lo, style='cyan')
 		exit()
 	try:
-		koh2 = requests.get('https://graph.facebook.com/'+pil+'/friends?limit=5000&access_token='+token)
+		koh2 = requests.get('https://graph.facebook.com/%s?fields=friends.limit(5001)&access_token=%s)
 		koh3 = json.loads(koh2.text)
 		for pi in koh3['data']:
 			try:id.append(pi['id']+'|'+pi['name'])
