129e71bde5b3b8eded53d3c807a1dc0d3ce4cdf6
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-06-09 15:49:47 +0700

    Update run.py

--- run.py
+++ run.py
@@ -918,7 +918,6 @@ def passwrd():
 				idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
 			frs = nmf.split(' ')[0]
 			pwv = ['cintaku','terserah','rahasia','indonesia','jancok','kontol','jancok123','kontol123','sayang','sayangku','sayang123','bismillah','anjing','katasandi','katasandi123','anjing123']
-			if len(nmf)<3:
 				if len(frs)<3::
 					pass
 
